7bde4215b1377f67a5c1bb4c0bc9b3a76b92f8b1
Felix Ruiz <chrisu2dell@yahoo.com>
2017-01-14 12:47:24 -0600

    Initial driver layout

---
+++ spark_m.c
@@ -0,0 +1,22 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/timer.h>
+#include <linux/input.h>
+#include <linux/io.h>
+#include <linux/interrupt.h>
+#include <linux/param.h>
+#include <linux/sched.h>
+
+
+#define DRV_NAME "sparkfun_hmb"
+
+//Register functions
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Team Station");
+MODULE_DESCRIPTION("Homebrew dirver for linux diploma curse");
+MODULE_VERSION("0.1");
+
+static int sparkfun_hmb_open()
+{
+
+}
