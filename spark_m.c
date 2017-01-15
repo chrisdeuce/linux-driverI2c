990985a11ee885b0fba1a2845ea41fd4f978f2fd
Felix Ruiz <chrisu2dell@yahoo.com>
2017-01-14 19:04:16 -0600

    spark_m.c

--- spark_m.c
+++ spark_m.c
@@ -6,17 +6,24 @@
 #include <linux/interrupt.h>
 #include <linux/param.h>
 #include <linux/sched.h>
+#include <linux/fs.h>
+#include <uasm/uacces.h>  //used to move data to from kernel to user space
+#include <linux/spinlock.h>
+#include <asm/aotmic.h>
+
 
 
 #define DRV_NAME "sparkfun_hmb"
 
 //Register functions
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Team Station");
-MODULE_DESCRIPTION("Homebrew dirver for linux diploma curse");
+MODULE_AUTHOR("Team StationX");
+MODULE_DESCRIPTION("Homebrew driver for linux diploma curse");
 MODULE_VERSION("0.1");
 
 static int sparkfun_hmb_open()
 {
 
 }
+
+static atomic 
