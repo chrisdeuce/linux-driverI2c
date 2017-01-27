c702f0512c0f49d8687ec3b7a3e0ad38257eb83e
Felix Ruiz <chrisu2dell@yahoo.com>
2017-01-27 11:15:43 -0600

    Finalizando registro del driver en el kernel

--- spark_m.c
+++ spark_m.c
@@ -2,7 +2,6 @@
 Información del driver
 Driver para utilizar la estación meteorologica I2C sparkfun weathershield 
 */
-
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/timer.h>
@@ -14,7 +13,9 @@ Driver para utilizar la estación meteorologica I2C sparkfun weathershield
 #include <linux/fs.h>
 #include <uasm/uacces.h>  //used to move data to from kernel to user space
 #include <linux/spinlock.h>
+#include <linux/semaphore.h>
 #include <asm/atomic.h>
+#include <asm/uaccess.h> // copy to user ; copy from user
 
 //#define DRV_NAME "spark"
 
@@ -24,14 +25,16 @@ MODULE_AUTHOR("Team StationX_FRM");
 MODULE_DESCRIPTION("Homebrew driver for sparkfun weather station");
 MODULE_VERSION("0.1");
 
-/*Creando la estructura del dispositivo*/
+/*
+ 1.  Creando la estructura del dispositivo
+*/
 struct spark_device {
   char data[100];
   struct data[100];
 }virtual device;
 
 /*
-Registrando el dispositivo
+2. Registrando el dispositivo
 */
 struct cdev *sparkdev
 int major_number;
@@ -40,6 +43,33 @@ int ret;
 dev_t dev_num;
 
 #define DEVICE_NAME "sparkfun"
+/*
+3. Registrando el dispositivo
+*/
+
+static int driver_entry(void){
+  //Paso 1, usar asignación automática, utilizando major_number
+  ret = alloc_chrdev_region(&dev_num,0,1,DEVICE_NAME);
+  if(ret<0){ /* verificamos si hay error al registrar el dispositivo*/
+    printk(KERN_ALERT "Sparkfun failed to allocate a major number");
+    return ret;
+  }
+  major_number = MAJOR(dev_num); //extrae el número mayor que se almacena en la variable (MACRO)
+  printk(KERN_INFO "Sparkfun: major number is: %d",major_number);
+  printk(KERN_INFO "\tuse \"mkmod /dev/%s c %d 0\"for device file",DEVICE_NAME,major_number); //dmesg
+  //Paso 2, crear la estructura cdev
+  mcdev =cdev_alloc();
+  mcdev->ops=&fops;
+  mcdev-owner = THIS_MODULE;
+  /*creación de cdev*/
+  ret =cdev_add(mcdev,dev_num,1);
+  if(ret<0){
+      printk(KERN_ALERT "Sparkfun: incapaz de agregar cdev en el kernel")
+      return ret;
+  }
+  return 0;
+}
+
 
 
 /*Iniciando el driver*/
